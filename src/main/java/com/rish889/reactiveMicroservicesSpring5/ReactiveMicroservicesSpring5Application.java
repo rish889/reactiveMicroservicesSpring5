05c5f56a3b2d2d25d0843e00ccfbacf409fe9c7b
Rishabh Garcha <rishabhg@amazon.com>
2019-01-03 14:23:02 +0530

    SpringBoot

---
+++ src/main/java/com/rish889/reactiveMicroservicesSpring5/ReactiveMicroservicesSpring5Application.java
@@ -0,0 +1,14 @@
+package com.rish889.reactiveMicroservicesSpring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMicroservicesSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMicroservicesSpring5Application.class, args);
+	}
+
+}
+
